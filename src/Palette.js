4ab1cb88d23d241443efe58488e1e1d9d9fb851c
Raj Karmakar <97rajmath@gmail.com>
2021-06-12 21:28:07 +0530

    Started with color palettes

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="palette">
+                {/* Navbar */}
+                <div className="palette-colors">
+                    {/* Color Boxes will appear here */}
+                </div>
+                {/* Footer */}
+            </div>
+        )
+    }
+}
+
+export default Palette
